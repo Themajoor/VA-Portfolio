c941614e3ccba8679f579324600c119ba31db5ad
Majaro Promise <110162390+Themajoor@users.noreply.github.com>
2025-03-28 19:03:28 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const links = document.querySelectorAll('a[href^="#"]');
+
+links.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    window.scrollTo({
+      top: target.offsetTop - 50,
+      behavior: "smooth",
+    });
+  });
+});
